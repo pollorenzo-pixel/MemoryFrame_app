181163ce0ed6344d95ecafef9c17ab029547ac67
pollorenzo-pixel <pollorenzo103093@gmail.com>
2026-06-22 14:55:59 +0100

    Wrap app with error boundary

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,19 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import AppErrorBoundary from './components/AppErrorBoundary.jsx'
 import './styles/global.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode><App /></StrictMode>,
+const rootElement = document.getElementById('root')
+
+if (!rootElement) {
+  throw new Error('MemoryFrame root element was not found')
+}
+
+createRoot(rootElement).render(
+  <StrictMode>
+    <AppErrorBoundary>
+      <App />
+    </AppErrorBoundary>
+  </StrictMode>,
 )
